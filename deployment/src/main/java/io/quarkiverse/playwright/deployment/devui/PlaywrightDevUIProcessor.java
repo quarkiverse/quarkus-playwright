e19c8120cc16953d9ee90678806302036104d661
melloware <mellowaredev@gmail.com>
2025-12-08 10:13:09 -0500

    Quarkus 3.27.1

--- deployment/src/main/java/io/quarkiverse/playwright/deployment/devui/PlaywrightDevUIProcessor.java
+++ deployment/src/main/java/io/quarkiverse/playwright/deployment/devui/PlaywrightDevUIProcessor.java
@@ -1,13 +1,9 @@
 package io.quarkiverse.playwright.deployment.devui;
 
-import com.microsoft.playwright.Playwright;
-
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
-import io.quarkus.devui.spi.page.ExternalPageBuilder;
-import io.quarkus.devui.spi.page.Page;
 
 /**
  * Dev UI card for displaying important details such as the Playwright library version.
@@ -17,17 +13,8 @@ public class PlaywrightDevUIProcessor {
     @BuildStep(onlyIf = IsDevelopment.class)
     void createVersion(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer) {
         final CardPageBuildItem card = new CardPageBuildItem();
-
-        final ExternalPageBuilder versionPage = Page.externalPageBuilder("Playwright Version")
-                .icon("font-awesome-solid:tag")
-                .url("https://playwright.dev")
-                .doNotEmbed()
-                .staticLabel(Playwright.class.getPackage().getImplementationVersion());
-
-        card.addPage(versionPage);
-
-        card.setCustomCard("qwc-playwright-card.js");
-
+        card.addLibraryVersion("com.microsoft.playwright", "playwright", "Playwright",
+                "https://playwright.dev");
         cardPageBuildItemBuildProducer.produce(card);
     }
 }
