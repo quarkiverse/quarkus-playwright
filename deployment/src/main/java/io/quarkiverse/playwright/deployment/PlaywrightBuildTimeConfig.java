78a6d765046d250effb9ca0a64aecfbf3e444f48
Melloware <mellowaredev@gmail.com>
2026-09-18 10:14:15 -0400

    Update Playwright default image version to v1.63.0

--- deployment/src/main/java/io/quarkiverse/playwright/deployment/PlaywrightBuildTimeConfig.java
+++ deployment/src/main/java/io/quarkiverse/playwright/deployment/PlaywrightBuildTimeConfig.java
@@ -17,7 +17,7 @@ public interface PlaywrightBuildTimeConfig {
 
     @ConfigGroup
     interface PlaywrightDevServicesConfig {
-        String DEFAULT_IMAGE = "mcr.microsoft.com/playwright:v1.61.0-noble";
+        String DEFAULT_IMAGE = "mcr.microsoft.com/playwright:v1.63.0-resolute";
 
         /**
          * Starts a Playwright server container in dev and test modes.
